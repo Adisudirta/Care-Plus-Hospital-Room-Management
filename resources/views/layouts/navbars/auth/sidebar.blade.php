28c9c2929d0f72d74ea8a7721dba4e6c0368cafc
Adisudirta <yanadisudirta@gmail.com>
2022-12-31 08:36:34 +0800

    🪄 Improve UI at admin profile page and side bar

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -59,7 +59,7 @@
             <li class="nav-item pb-2">
                 <a class="nav-link {{ (Request::is('antrian') ? 'active' : '') }}" href="{{ url('antrian') }}">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i style="font-size: 1rem;" class="fas fa-lg fa-list-ul ps-2 pe-2 text-center text-dark {{ (Request::is('user-management') ? 'text-white' : 'text-dark') }} " aria-hidden="true"></i>
+                        <i style="font-size: 1rem;" class="fas fa-lg fa-list-ul ps-2 pe-2 text-center text-dark {{ (Request::is('antrian') ? 'text-white' : 'text-dark') }} " aria-hidden="true"></i>
                     </div>
                     <span class="nav-link-text ms-1">Antrian</span>
                 </a>
